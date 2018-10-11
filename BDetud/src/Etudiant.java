ee5dfa2111aa8e0f2712126119288d4c24ac25b1
hafsaouchra <ouchrahafsa43@gmail.com>
2018-10-11 19:02:13 +0100

    the first test de git in eclipse

---
+++ BDetud/src/Etudiant.java
@@ -0,0 +1,37 @@
+
+public class Etudiant {
+	private char cne;
+	private char nom;
+	private char prenom;
+	
+	public char getCne() {
+		return cne;
+	}
+	public void setCne(char cne) {
+		this.cne = cne;
+	}
+	public char getNom() {
+		return nom;
+	}
+	public void setNom(char nom) {
+		this.nom = nom;
+	}
+	public char getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(char prenom) {
+		this.prenom = prenom;
+	}
+	public Etudiant(char cne, char nom, char prenom) {
+		super();
+		this.cne = cne;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	@Override
+	public String toString() {
+		return "Etudiant [cne=" + cne + ", nom=" + nom + ", prenom=" + prenom + "]";
+	}
+	
+	
+}
